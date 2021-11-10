c2a872a75c9b4d903445c81b4321c939614b3ac2
gokhan721 <simsek.gokhan721@gmail.com>
2021-11-10 15:43:52 +0300

    fix pip and pytest check

--- src/actions/pytest/index.ts
+++ src/actions/pytest/index.ts
@@ -8,14 +8,10 @@ export default async function run(thundraDep: string) {
     const check_pip_installed = await Helper.checkPipInstallation()
     if (!check_pip_installed) {
         core.warning('[THUNDRA] To install Thundra, pip is required. Please ensure your workspace has pip...')
-        core.warning('[THUNDRA] Instrumentation failed due to missing pip...')
-        process.exit(core.ExitCode.Success)
     }
     const check_pytest_installed = await Helper.checkPytestInstallation()
     if (!check_pytest_installed) {
         core.warning('[THUNDRA] To install Thundra, pytest is required. Please ensure your workspace has pytest...')
-        core.warning('[THUNDRA] Instrumentation failed due to missing pytest...')
-        process.exit(core.ExitCode.Success)
     }
     await Helper.installThundraPythonAgent(thundraDep)
     core.info(`[THUNDRA] ${thundraDep} installed...`)
